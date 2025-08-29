fe117443cf2bb31629c2cda0a088733c0af2bac9
xwaspy <skytechx3@gmail.com>
2025-08-29 15:43:39 +0300

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -16,38 +16,43 @@ const Experience = () => {
               Where it was achived
             </p>
             <p className="text-sm text-gray-600">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptatibus porro, unde maxime atque in cum reiciendis voluptate
-              repellendus reprehenderit facilis velit enim dolorum? Laudantium
-              vel maiores mollitia consequatur voluptatem numquam.
+              Along university, I have worked on several projects that have
+              allowed me to apply and enhance my web development skills. These
+              projects range from personal websites to collaborative team
+              efforts, showcasing my ability to create responsive and
+              user-friendly web applications.
             </p>
             <span className="absolute -left-[11px] top-10 h-5 w-5 rounded-full bg-purple-500"></span>
           </div>
 
           <div className="relative space-y-1 border-l-2 p-8 text-left">
-            <h3 className="text-xl font-semibold md:text-2xl ">Experience 1</h3>
+            <h3 className="text-xl font-semibold md:text-2xl ">Experience 2</h3>
             <p className="font-light text-lg text-gray-600">
               Where it was achived
             </p>
             <p className="text-sm text-gray-600">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptatibus porro, unde maxime atque in cum reiciendis voluptate
-              repellendus reprehenderit facilis velit enim dolorum? Laudantium
-              vel maiores mollitia consequatur voluptatem numquam.
+              While freelancing, I have had the opportunity to work with diverse
+              clients, delivering tailored web solutions that meet their
+              specific needs. This experience has honed my skills in client
+              communication, project management, and adapting to various project
+              requirements. As a web developer, I am committed to continuous
+              learning and staying updated with the latest industry trends and
+              technologies to provide the best solutions for my clients.
             </p>
             <span className="absolute -left-[11px] top-10 h-5 w-5 rounded-full bg-purple-500"></span>
           </div>
 
           <div className="relative space-y-1 border-l-2 p-8 text-left">
-            <h3 className="text-xl font-semibold md:text-2xl ">Experience 1</h3>
+            <h3 className="text-xl font-semibold md:text-2xl ">Experience 3</h3>
             <p className="font-light text-lg text-gray-600">
               Where it was achived
             </p>
             <p className="text-sm text-gray-600">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptatibus porro, unde maxime atque in cum reiciendis voluptate
-              repellendus reprehenderit facilis velit enim dolorum? Laudantium
-              vel maiores mollitia consequatur voluptatem numquam.
+              For personal projects, I have developed a variety of web
+              applications that reflect my creativity and technical skills.
+              These projects serve as a testament to my ability to independently
+              manage and execute web development tasks, from initial concept to
+              final deployment.
             </p>
             <span className="absolute -left-[11px] top-10 h-5 w-5 rounded-full bg-purple-500"></span>
           </div>
