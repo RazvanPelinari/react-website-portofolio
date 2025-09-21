8567849c9473fd817f7ac06c445f0e787ca014f7
xwaspy <skytechx3@gmail.com>
2025-09-21 14:40:30 +0300

    Update

--- hooks/useScrollMagic.js
+++ hooks/useScrollMagic.js
@@ -1,21 +1,21 @@
-// src/hooks/useScrollMagic.js
 import { useEffect } from "react";
 import ScrollMagic from "scrollmagic";
-import { TweenMax } from "gsap";
 
 export default function useScrollMagic() {
   useEffect(() => {
     const controller = new ScrollMagic.Controller();
 
-    // Fade-in elements
     document.querySelectorAll(".scroll-anim").forEach((el) => {
       new ScrollMagic.Scene({
         triggerElement: el,
-        triggerHook: 0.9,
-        reverse: false,
+        triggerHook: 0.8,
+        duration: "80%",
+        reverse: true, // 👈 allow toggle when leaving
       })
-        .setClassToggle(el, "visible")
+        .setClassToggle(el, "visible") // adds AND removes
         .addTo(controller);
     });
+
+    return () => controller.destroy(true);
   }, []);
 }
