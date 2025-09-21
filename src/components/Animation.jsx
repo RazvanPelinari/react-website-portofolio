54efcfb26bf95c9fe05e4a420aa25b21fd166504
Razvan Pelinari <skytechx3@gmail.com>
2025-09-22 00:24:42 +0300

    update

--- src/components/Animation.jsx
+++ src/components/Animation.jsx
@@ -7,7 +7,7 @@ export default function Section({ children, bg = "bg-white" }) {
       initial={{ opacity: 0, y: 50 }} // start faded & shifted down
       whileInView={{ opacity: 1, y: 0 }} // animate when visible
       exit={{ opacity: 0.2, y: 50 }} // animate out when leaving
-      viewport={{ once: false, amount: 0.5 }}
+      viewport={{ once: false, amount: 0.7}}
       transition={{ duration: 0.5, ease: "easeOut" }}
       className={`min-h-screen flex items-center justify-center ${bg}`}
     >
