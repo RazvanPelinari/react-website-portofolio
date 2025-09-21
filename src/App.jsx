d1589f3737391e37311c94d277e606956eb36d1e
xwaspy <skytechx3@gmail.com>
2025-09-21 14:59:14 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import More from "./components/More";
-import useScrollMagic from "./hooks/useScrollMagic";
+import useScrollMagic from "../hooks/useScrollMagic";
 
 function App() {
   useScrollMagic();
