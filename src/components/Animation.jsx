14dbd9d3c00a10201fac1f62d1a7616e392f5396
Razvan Pelinari <skytechx3@gmail.com>
2025-09-22 00:30:03 +0300

    update

--- src/components/Animation.jsx
+++ src/components/Animation.jsx
@@ -4,10 +4,10 @@ import { motion } from "framer-motion";
 export default function Section({ children, bg = "bg-white" }) {
   return (
     <motion.section
-      initial={{ opacity: 0, y: 50 }} // start faded & shifted down
+      initial={{ opacity: 0, y: 0}} // start faded & shifted down
       whileInView={{ opacity: 1, y: 0 }} // animate when visible
-      exit={{ opacity: 0.2, y: 50 }} // animate out when leaving
-      viewport={{ once: false, amount: 0.7}}
+      exit={{ opacity: 0.1, y: 0 }} // animate out when leaving
+      viewport={{ once: false, amount: 0.2}}
       transition={{ duration: 0.5, ease: "easeOut" }}
       className={`min-h-screen flex items-center justify-center ${bg}`}
     >
