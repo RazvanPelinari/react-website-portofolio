2582069aeb6d058183f6b2e50747eeabe0931ddc
Razvan Pelinari <skytechx3@gmail.com>
2025-09-22 00:36:57 +0300

    update

--- src/components/Animation.jsx
+++ src/components/Animation.jsx
@@ -1,13 +1,16 @@
 // src/components/Section.jsx
-import { motion } from "framer-motion";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
 
 export default function Section({ children, bg = "bg-white" }) {
+  const ref = useRef(null);
+  const inView = useInView(ref, { amount: 0.1, once: false });
+
   return (
     <motion.section
-      initial={{ opacity: 0, y: 0}} // start faded & shifted down
-      whileInView={{ opacity: 1, y: 0 }} // animate when visible
-      exit={{ opacity: 0.1, y: 0 }} // animate out when leaving
-      viewport={{ once: false, amount: 0.2}}
+      ref={ref}
+      initial={{ opacity: 0, y: 50 }}
+      animate={inView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
       transition={{ duration: 0.5, ease: "easeOut" }}
       className={`min-h-screen flex items-center justify-center ${bg}`}
     >
