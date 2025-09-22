87d88ee7d3bda0f1f1bc7aaa718fb87b12ee28c6
xwaspy <skytechx3@gmail.com>
2025-09-22 14:49:07 +0300

    Update

--- src/components/Animation.jsx
+++ src/components/Animation.jsx
@@ -10,7 +10,7 @@ export default function Section({ children, bg = "bg-white" }) {
     <motion.section
       ref={ref}
       initial={{ opacity: 0, y: 0 }}
-      animate={inView ? { opacity: 1, y: 0 } : { opacity: 0, y: 0 }}
+      animate={inView ? { opacity: 1, y: 50 } : { opacity: 0, y: 50 }}
       transition={{ duration: 0.5, ease: "easeOut" }}
       className={`min-h-screen flex items-center justify-center ${bg}`}
     >
