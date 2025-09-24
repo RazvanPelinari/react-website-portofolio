151759ddfbb9d6d697d503a45f4703bbf702f065
xwaspy <skytechx3@gmail.com>
2025-09-24 13:16:12 +0300

    Update ScrollAnimation.jsx

--- src/components/Animations/ScrollAnimation.jsx
+++ src/components/Animations/ScrollAnimation.jsx
@@ -4,18 +4,18 @@ import { useRef } from "react";
 
 export default function Section({ children, bg = "bg-white" }) {
   const ref = useRef(null);
-  const inView = useInView(ref, { amount: 0.2, once: false });
+  const inView = useInView(ref, { amount: 0.1, once: false });
 
   return (
     <motion.section
       ref={ref}
-      initial={{ opacity: 0, y: 0, scale: 1 }}
+      initial={{ opacity: 0.1, y: 0, scale: 1 }}
       animate={
         inView
           ? { opacity: 1, y: 50, scale: 1.0 }
           : { opacity: 0, y: 50, scale: 1.1 }
       }
-      transition={{ duration: 0.2, ease: "easeOut" }}
+      transition={{ duration: 0.3, ease: "easeOut" }}
       className={`min-h-screen w-full flex items-center justify-center ${bg}`}
     >
       {children}
